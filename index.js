5cba3d6938f36f2d2dd82e3d4e29223795b3f489
anxmukul <kmukul1197@gmail.com>
2021-12-24 22:39:51 +0530

    Connecting to postgres databse

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 const express = require('express');
+const {Client} = require('pg');
 const port = 3000;
-
 const app = express();
-
+var client = new Client({
+    user: "postgres",
+    host: "localhost",
+    database: "asitysignup",
+    password: "blah",
+    port: 5432,
+})
 app.get('/', (req, res) => {
     res.send("welcome to the magic world -> Asity");
 })
 
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
+    console.log(`Example app listening at http://localhost:${port}`);
+    client.connect();
+    console.log("Database connection: Successful");
 })
