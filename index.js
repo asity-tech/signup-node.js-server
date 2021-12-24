f1faed8af8702e1ad1077167c9de49ec6fc1c15f
anxmukul <kmukul1197@gmail.com>
2021-12-24 21:53:09 +0530

    creating main route

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const port = 3000;
 
-
 const app = express();
+
+app.get('/', (req, res) => {
+    res.send("welcome to the magic world -> Asity");
+})
+
+
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
 })
