1c49a9c8768edf2790d187dff9f7706f9c50b383
anxmukul <kmukul1197@gmail.com>
2021-12-25 11:23:08 +0530

    Installing body-parser to varify the data in body of req

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const express = require('express');
 const res = require('express/lib/response');
 const {Client} = require('pg');
 const port = 3000;
+var bodyParser = require('body-parser');
 const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
 var client = new Client({
     user: process.env.user,
     host: process.env.host,
@@ -15,17 +17,15 @@ app.get('/', (req, res) => {
 })
 
 app.post('/signup', (req, res) => {
-    let emailid = req.body.Email
-    let passcode = req.body.Password
-    const insertquery = `insert into accounts(email, password) values('${emailid}', '${password}')`;
+    var emailid = req.body.Email
+    var passcode = req.body.Password
+    const insertquery = `insert into accounts(email, password) values('${emailid}', '${passcode}')`;
     client.query(insertquery, (err, result) => {
         if(err){
             res.send('Error in inserting into Db', err);
         }
         else{
             res.status(201);
-            res.send(result);
-            console.log('Insertion in Db: Successful');
         }
     });
 });
