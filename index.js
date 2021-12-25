5a7873f0e3e03f3829541f00eedee822bc731c9f
anxmukul <kmukul1197@gmail.com>
2021-12-25 10:54:19 +0530

    Creating signup route using post method

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const res = require('express/lib/response');
 const {Client} = require('pg');
 const port = 3000;
 const app = express();
@@ -13,9 +14,29 @@ app.get('/', (req, res) => {
     res.send("welcome to the magic world -> Asity");
 })
 
+app.post('/signup', (req, res) => {
+    let emailid = req.body.Email
+    let passcode = req.body.Password
+    const insertquery = `insert into accounts(email, password) values('${emailid}', '${password}')`;
+    client.query(insertquery, (err, result) => {
+        if(err){
+            res.send('Error in inserting into Db', err);
+        }
+        else{
+            res.status(201);
+            res.send(result);
+            console.log('Insertion in Db: Successful');
+        }
+    });
+});
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
-    client.connect();
-    console.log("Database connection: Successful");
+    client.connect((err) => {
+        if(err){
+           throw err;
+        }
+        console.log("Database connection: Successful");
+    });
+    
 })
