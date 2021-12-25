1e9646ce01c56a7bc68013ba4e7d2804779e5ad7
anxmukul <kmukul1197@gmail.com>
2021-12-25 13:10:37 +0530

    Using body-parser for json raw data in header file and adding function to remove whitespaces

--- index.js
+++ index.js
@@ -1,31 +1,50 @@
 const express = require('express');
 const res = require('express/lib/response');
 const {Client} = require('pg');
-const port = 3000;
 var bodyParser = require('body-parser');
+const port = 3000;
 const app = express();
-app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json())
+
 var client = new Client({
-    user: process.env.user,
-    host: process.env.host,
-    database: process.env.database,
-    password: process.env.password,
+    user: process.env.user || 'postgres',
+    host: process.env.host|| 'localhost',
+    database: process.env.database || 'asitysignup',
+    password: process.env.password || 'blah',
     port: 5432,
 })
+
+function removeWhiteSpaceFromEnd(title) {
+    var size = title.length;
+    while (title[size - 1] == " ") {
+      size--;
+    }
+    var temp = "";
+    for (let j = 0; j < size; j++) {
+      temp += title[j];
+    }
+    return temp;
+}
+
 app.get('/', (req, res) => {
     res.send("welcome to the magic world -> Asity");
 })
 
 app.post('/signup', (req, res) => {
-    var emailid = req.body.Email
-    var passcode = req.body.Password
+    var rawEmail = req.body.Email
+    var emailid = removeWhiteSpaceFromEnd(rawEmail);
+    var rawPasscode = req.body.Password
+    var passcode = removeWhiteSpaceFromEnd(rawPasscode);
     const insertquery = `insert into accounts(email, password) values('${emailid}', '${passcode}')`;
     client.query(insertquery, (err, result) => {
         if(err){
-            res.send('Error in inserting into Db', err);
+            res.send(err.message || err);
         }
         else{
             res.status(201);
+            console.log('Db insertion successful');
+            res.send(result);
         }
     });
 });
