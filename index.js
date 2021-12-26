181db5e8e33980bee20e3b288132e11b3d027afb
anxmukul <kmukul1197@gmail.com>
2021-12-26 23:29:27 +0530

    Adding .envFile to save env variable for debugger to run smoothly without error

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const res = require('express/lib/response');
 const {Client} = require('pg');
 var bodyParser = require('body-parser');
 const { json } = require('body-parser');
-const port = 3001;
+const port = 3000;
 const app = express();
 // app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json())
